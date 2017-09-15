bca847e8fd1e411eca1702e0cc8e01a7d7d2074d
fubao <freadream@gmail.com>
2017-09-15 01:03:01 -0400

    update_function

--- run_me.py
+++ run_me.py
@@ -3,50 +3,62 @@ import numpy as np
 import kaggle
 
 
-class predictLR:
+class clsregressionHw(object):
  
     
     def __init__(self):
       pass
 
 
-# Read in train and test data
-def read_data_power_plant():
-	print('Reading power plant dataset ...')
-	train_x = np.loadtxt('../../Data/PowerOutput/data_train.txt')
-	train_y = np.loadtxt('../../Data/PowerOutput/labels_train.txt')
-	test_x = np.loadtxt('../../Data/PowerOutput/data_test.txt')
-
-	return (train_x, train_y, test_x)
-
-def read_data_localization_indoors():
-	print('Reading indoor localization dataset ...')
-	train_x = np.loadtxt('../../Data/IndoorLocalization/data_train.txt')
-	train_y = np.loadtxt('../../Data/IndoorLocalization/labels_train.txt')
-	test_x = np.loadtxt('../../Data/IndoorLocalization/data_test.txt')
-
-	return (train_x, train_y, test_x)
-
-# Compute MAE
-def compute_error(y_hat, y):
-	# mean absolute error
-	return np.abs(y_hat - y).mean()
+    # Read in train and test data
+    def read_data_power_plant(self):
+    	print('Reading power plant dataset ...')
+    	train_x = np.loadtxt('../../Data/PowerOutput/data_train.txt')
+    	train_y = np.loadtxt('../../Data/PowerOutput/labels_train.txt')
+    	test_x = np.loadtxt('../../Data/PowerOutput/data_test.txt')
+    
+    	return (train_x, train_y, test_x)
+    
+    def read_data_localization_indoors(self):
+    	print('Reading indoor localization dataset ...')
+    	train_x = np.loadtxt('../../Data/IndoorLocalization/data_train.txt')
+    	train_y = np.loadtxt('../../Data/IndoorLocalization/labels_train.txt')
+    	test_x = np.loadtxt('../../Data/IndoorLocalization/data_test.txt')
+    
+    	return (train_x, train_y, test_x)
+    
+    # Compute MAE
+    def compute_error(y_hat, y):
+    	# mean absolute error
+    	return np.abs(y_hat - y).mean()
 
 ############################################################################
 
-train_x, train_y, test_x = read_data_power_plant()
-print('Train=', train_x.shape)
-print('Test=', test_x.shape)
 
-train_x, train_y, test_x = read_data_localization_indoors()
-print('Train=', train_x.shape)
-print('Test=', test_x.shape)
 
-# Create dummy test output values
-predicted_y = np.ones(test_x.shape[0]) * -1
-# Output file location
-file_name = '../Predictions/IndoorLocalization/best.csv'
-# Writing output in Kaggle format
-print('Writing output to ', file_name)
-kaggle.kaggleize(predicted_y, file_name)
+def main():
+    
+    regrHwObj = clsregressionHw()
+    
+    train_x, train_y, test_x = regrHwObj.read_data_power_plant()
+    print('Train=', train_x.shape)
+    print('Test=', test_x.shape)
+    
+    train_x, train_y, test_x = regrHwObj.read_data_localization_indoors()
+    print('Train=', train_x.shape)
+    print('Test=', test_x.shape)
+    
+    # Create dummy test output values
+    predicted_y = np.ones(test_x.shape[0]) * -1
+    # Output file location
+    file_name = '../Predictions/IndoorLocalization/best.csv'
+    # Writing output in Kaggle format
+    print('Writing output to ', file_name)
+    kaggle.kaggleize(predicted_y, file_name)
+
+
+if __name__== "__main__":
+  main()
+  
+
 
